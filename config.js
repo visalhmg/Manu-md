21266d41a9358530e2277b8e75cb11a4ab9ea467
visalhmg <66855760+visalhmg@users.noreply.github.com>
2025-06-19 18:13:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,6 +41,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Put_Your_Session_ID', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&zdcHDKAI#g-27v4ri212W_eaURHf-dS3Ibo1ItL0Lcwsp7soGfbk', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000'
 };
